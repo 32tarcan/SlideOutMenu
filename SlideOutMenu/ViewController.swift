094efd64cfd022d29c5928bda477d037b78cc12b
32tarcan <bahadir.tarcan41@gmail.com>
2023-06-02 19:11:45 +0300

    Initial Commit

---
+++ SlideOutMenu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SlideOutMenu
+//
+//  Created by Bahadır Tarcan on 2.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
